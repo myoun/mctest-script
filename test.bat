a0bb209515ec246b59f094ed12259ac59f78ab6a
myoun <aiden080605@gmail.com>
2021-09-12 21:03:22 +0900

    update

--- test.bat
+++ test.bat
@@ -1,7 +1,5 @@
-gradlew build build
-
 if exist mctest-script/paper-1.17.1.jar (
     java -jar mctest-script/paper-1.17.1.jar nogui
 ) else (
-    python download.py -v 1.17.1 -d
+    python mctest/download.py -v 1.17.1 -d
 )
