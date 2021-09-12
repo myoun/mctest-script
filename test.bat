e30717302a057510e23664a2443ea8033224a0e3
myoun <aiden080605@gmail.com>
2021-09-12 21:07:52 +0900

    script

--- test.bat
+++ test.bat
@@ -1,5 +1,4 @@
-if exist mctest-script/paper-1.17.1.jar (
-    java -jar mctest-script/paper-1.17.1.jar nogui
-) else (
-    python mctest/download.py -v 1.17.1 -d
+if not exist mctest-script/paper-1.17.1.jar (
+    python mctest-script/download.py -v 1.17.1 -d
 )
+java -jar mctest-script/paper-1.17.1.jar nogui
